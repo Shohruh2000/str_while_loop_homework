3785a0e9b5da22241cf8ebf08a9135a0981def45
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-03 06:42:54 +0500

    While07.py

--- While07.py
+++ While07.py
@@ -1,3 +1,5 @@
+
+
 def main(s):
     """
     A string of numbers is given. Find how many even numbers there are and return.
@@ -6,4 +8,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    x = int(s)
+    k = 0
+    while x != 0:
+        y = x%10
+
+        if y%2==0:
+            k +=1
+        x //=10
+    return k
+print(main("12345688"))
+    
