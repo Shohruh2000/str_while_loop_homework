2de25d096af9ada1d69abc7198246bfe7c313e42
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-03 07:14:13 +0500

    While03.py

--- While03.py
+++ While03.py
@@ -6,4 +6,15 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    k = 0
+    j = 0
+    while i < len(s):
+        if s[i].isdigit():
+            k +=1
+        elif s[i].isalpha():
+            j +=1
+        i = i+1
+        
+    return (len(s)-k-j)
+print(main("12@#sd*12"))
